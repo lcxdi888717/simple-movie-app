4c627c869305b1dbcf33898866b6ea9ff94f9dfa
LoveYancy <lcxdi8887@gmail.com>
2018-12-09 15:01:18 +1100

    Setup basic layout and modify layout styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout } from "antd";
+import React, { Component } from "react";
+
+import { LeftSider } from "./views";
+
+const { Sider, Header, Content, Footer } = Layout;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Layout>
+        <Sider width={300} breakpoint="lg" collapsedWidth="0">
+          <LeftSider />
+        </Sider>
+        <Layout>
+          <Header>header</Header>
+          <Content>content</Content>
+          <Footer>footer</Footer>
+        </Layout>
+      </Layout>
     );
   }
 }
